88d4b470df03f2b3056522c050587550f2d3dbc6
Yusif Badmus <yusif.badmus@gmail.com>
2023-12-27 19:15:19 +0000

    initial devise setup

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+Rails.application.routes.draw do
+  get 'dashboard/index'
+  devise_for :users
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  root to: 'dashboard#index'
+end
