f927018804ee3ce1356d3d84d8836fc466594ff6
Yusif Badmus <yusif.badmus@gmail.com>
2024-01-04 23:27:32 +0000

    add incoterms view

---
+++ app/controllers/incoterms_controller.rb
@@ -0,0 +1,7 @@
+class IncotermsController < ApplicationController
+
+  # GET /incoterms or /incoterms.json
+  def index
+    @incoterms = Incoterm.all
+  end
+end
---
+++ test/controllers/incoterms_controller_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class IncotermsControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
