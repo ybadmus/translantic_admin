006e1ae66d7e8339b5943682f83f576e4594fb43
Yusif Badmus <yusif.badmus@gmail.com>
2024-01-06 14:28:18 +0000

    general house keeping

--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,10 @@
 #
 
 Rails.application.routes.draw do
-  resources :incoterms, only: :index
+  resources :incoterms, only: %i[index]
   resources :shipments
   resources :quotes
-  resources :locations
+  resources :locations, except: %i[show]
   resources :customers
   get 'dashboard/index'
   devise_for :users
--- test/models/location_test.rb
+++ test/models/location_test.rb
@@ -4,10 +4,10 @@
 #
 #  id         :bigint           not null, primary key
 #  city       :string(255)      not null
+#  code       :string(5)        default("")
 #  country    :string(255)      not null
 #  is_deleted :boolean          default(FALSE), not null
 #  state      :string(255)      default("")
-#  zip_code   :string(5)        default("")
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
 #
