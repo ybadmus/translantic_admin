d3d47e53ef1bff39680e706bd90cac07add54a73
Yusif Badmus <yusif.badmus@gmail.com>
2024-01-08 23:51:28 +0000

    add changes on test suite files

--- test/models/location_test.rb
+++ test/models/location_test.rb
@@ -5,11 +5,15 @@
 #  id         :bigint           not null, primary key
 #  city       :string(255)      not null
 #  code       :string(5)        default("")
-#  country    :string(255)      not null
+#  country    :string(255)      default("")
 #  is_deleted :boolean          default(FALSE), not null
 #  state      :string(255)      default("")
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
+#
+# Indexes
+#
+#  index_locations_on_city  (city) UNIQUE
 #
 require "test_helper"
 
