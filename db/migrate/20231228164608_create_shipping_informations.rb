d10f803bce3e77bcd82c00677ffb505c3dbb0979
Yusif Badmus <yusif.badmus@gmail.com>
2024-01-23 21:18:52 +0000

    fix migrations

--- db/migrate/20231228164608_create_shipping_informations.rb
+++ db/migrate/20231228164608_create_shipping_informations.rb
@@ -1,11 +1,9 @@
 class CreateShippingInformations < ActiveRecord::Migration[7.0]
   def change
     create_table :shipping_informations do |t|
-      t.references(:receiver, null: false, foreign_key: { to_table: 'customers' })
       t.string(:company_name)
       t.string(:address_line1, null: false)
       t.string(:address_line2)
-      t.references(:destination, null: false, foreign_key: { to_table: 'locations' })
       t.boolean(:is_deleted, default: false, null: false)
 
       t.timestamps
