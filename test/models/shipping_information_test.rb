b84737bbbaed411ce036b694e681edd85eee4b68
Yusif Badmus <yusif.badmus@gmail.com>
2024-01-23 21:21:45 +0000

    add changes for test files

--- test/models/shipping_information_test.rb
+++ test/models/shipping_information_test.rb
@@ -2,25 +2,13 @@
 #
 # Table name: shipping_informations
 #
-#  id             :bigint           not null, primary key
-#  address_line1  :string(255)      not null
-#  address_line2  :string(255)
-#  company_name   :string(255)
-#  is_deleted     :boolean          default(FALSE), not null
-#  created_at     :datetime         not null
-#  updated_at     :datetime         not null
-#  destination_id :bigint           not null
-#  receiver_id    :bigint           not null
-#
-# Indexes
-#
-#  index_shipping_informations_on_destination_id  (destination_id)
-#  index_shipping_informations_on_receiver_id     (receiver_id)
-#
-# Foreign Keys
-#
-#  fk_rails_...  (destination_id => locations.id)
-#  fk_rails_...  (receiver_id => customers.id)
+#  id            :bigint           not null, primary key
+#  address_line1 :string(255)      not null
+#  address_line2 :string(255)
+#  company_name  :string(255)
+#  is_deleted    :boolean          default(FALSE), not null
+#  created_at    :datetime         not null
+#  updated_at    :datetime         not null
 #
 require "test_helper"
 
