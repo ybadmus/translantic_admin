1e3bc63dac4b56c72dbfed0b94e60542ebf7f765
Yusif Badmus <yusif.badmus@gmail.com>
2024-01-23 21:22:17 +0000

    support dashboard link on /index route

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
   resources :quotes
   resources :locations, except: %i[show]
   resources :customers
+  resources :dashboard, only: %i[index]
   get 'dashboard/index'
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
