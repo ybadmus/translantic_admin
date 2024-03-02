23225fbf6fef876b7a06d982070a5971bad0e047
Yusif Badmus <yusif.badmus@gmail.com>
2024-03-02 00:23:41 +0000

    add audits controller destroy method

---
+++ app/controllers/audits_controller.rb
@@ -0,0 +1,18 @@
+class AuditsController < ApplicationController
+  before_action :set_audit, only: %i[ destroy ]
+
+  def destroy
+    @audit.destroy
+
+    respond_to do |format|
+      format.turbo_stream { render turbo_stream: turbo_stream.remove("#{helpers.dom_id(@audit)}_row") }
+      format.html { redirect_to shipments_url, notice: "Audit was successfully deleted." }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    def set_audit
+      @audit = Audited::Audit.find(params[:id])
+    end
+end
---
+++ test/controllers/audits_controller_test.rb
@@ -0,0 +1,8 @@
+require "test_helper"
+
+class AuditsControllerTest < ActionDispatch::IntegrationTest
+  test "should get destroy" do
+    get audits_destroy_url
+    assert_response :success
+  end
+end
