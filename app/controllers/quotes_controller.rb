6e6cb5fa3caea74aadca33d92a50c35110e40647
Yusif Badmus <yusif.badmus@gmail.com>
2024-03-04 10:03:22 +0000

    add pdf preview for quotes

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -1,7 +1,9 @@
+# frozen_string_literal: true
+
 class QuotesController < ApplicationController
-  before_action :set_quote, only: %i[ show edit update destroy ]
+  before_action :set_quote, only: %i[show edit update destroy pdf]
   before_action :set_quoter, only: :create
-  before_action :set_locations, only: %i[ create update ]
+  before_action :set_locations, only: %i[create update]
 
   # GET /quotes or /quotes.json
   def index
@@ -9,8 +11,7 @@ class QuotesController < ApplicationController
   end
 
   # GET /quotes/1 or /quotes/1.json
-  def show
-  end
+  def show; end
 
   # GET /quotes/new
   def new
@@ -19,7 +20,11 @@ class QuotesController < ApplicationController
   end
 
   # GET /quotes/1/edit
-  def edit
+  def edit; end
+
+  # GET /shipments/1/pdf
+  def pdf
+    render_pdf
   end
 
   # POST /quotes or /quotes.json
@@ -30,7 +35,7 @@ class QuotesController < ApplicationController
 
     respond_to do |format|
       if @quote.save
-        format.html { redirect_to new_quote_url, notice: "Quote was successfully created." }
+        format.html { redirect_to new_quote_url, notice: 'Quote was successfully created.' }
         format.json { render :show, status: :created, location: @quote }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -46,7 +51,7 @@ class QuotesController < ApplicationController
 
     respond_to do |format|
       if @quote.update(quote_params)
-        format.html { redirect_to quote_url(@quote), notice: "Quote was successfully updated." }
+        format.html { redirect_to quote_url(@quote), notice: 'Quote was successfully updated.' }
         format.json { render :show, status: :ok, location: @quote }
       else
         format.html { render :edit, status: :unprocessable_entity }
@@ -61,30 +66,46 @@ class QuotesController < ApplicationController
 
     respond_to do |format|
       format.turbo_stream { render turbo_stream: turbo_stream.remove("#{helpers.dom_id(@quote)}_row") }
-      format.html { redirect_to quotes_url, notice: "Quote was successfully destroyed." }
+      format.html { redirect_to quotes_url, notice: 'Quote was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_quote
-      @quote = Quote.find(params[:id])
-    end
 
-    def set_quoter
-      @quoter = Quoter.find_by(email: quote_params[:quoter_attributes][:email])
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_quote
+    @quote = Quote.find(params[:id])
+  end
 
-    # Only allow a list of trusted parameters through.
-    def quote_params
-      params.require(:quote).permit(:frieght_type, :height, :length, :width, :message, :status, :total_gross_weight, :incoterm_id, :destination_city, :departure_city,  quoter_attributes: %i[id name email phone])
-    end
+  def set_quoter
+    @quoter = Quoter.find_by(email: quote_params[:quoter_attributes][:email])
+  end
 
-    def quote_params_hash
-      quote_params_hash = quote_params.to_h
-      quote_params_hash[:quoter_id] = @quoter&.id
-      quote_params_hash[:quoter_attributes][:id] = @quoter&.id
-      quote_params_hash
-    end
+  # Only allow a list of trusted parameters through.
+  def quote_params
+    params.require(:quote).permit(:frieght_type, :height, :length, :width, :message, :status, :total_gross_weight,
+                                  :incoterm_id, :destination_city, :departure_city, quoter_attributes: %i[id name email phone])
+  end
+
+  def quote_params_hash
+    quote_params_hash = quote_params.to_h
+    quote_params_hash[:quoter_id] = @quoter&.id
+    quote_params_hash[:quoter_attributes][:id] = @quoter&.id
+    quote_params_hash
+  end
+
+  def render_pdf
+    render(
+      template: 'quotes/pdf',
+      pdf: 'invoice',
+      page_size: 'A4',
+      margin: { bottom: 22 },
+      footer: {
+        html: {
+          template: 'pdf_document_footer'
+        }
+      }
+    )
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,18 +7,34 @@ Rails.application.routes.draw do
   resources :incoterms, only: %i[index]
   resources :shipments do
     member do
-       get :itenary
-       get :pdf
+      get :itenary
+      get :pdf
     end
   end
+
   resources :audits, only: %i[destroy]
-  resources :quotes
+  resources :quotes do
+    member do
+      get :pdf
+    end
+  end
   resources :locations, except: %i[show]
   resources :customers
   resources :dashboard, only: %i[index]
   get 'dashboard/index'
   devise_for :users
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  namespace :api, defaults: { format: :json } do
+    namespace :v1 do
+      resources :shipments do
+        collection do
+          get :pdf
+        end
+      end
+      resources :enquiries, only: %i[create]
+    end
+  end
 
   # Defines the root path route ("/")
   root to: 'dashboard#index'
