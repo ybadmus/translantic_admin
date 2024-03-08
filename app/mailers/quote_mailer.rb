9182d92ac84f5fdead1ee97bfb55a37f3a94d58d
Yusif Badmus <yusif.badmus@gmail.com>
2024-03-08 11:08:13 +0000

    routine fixes

--- app/mailers/quote_mailer.rb
+++ app/mailers/quote_mailer.rb
@@ -1,15 +1,13 @@
-# frozen_string_literal: true
-
 class QuoteMailer < ApplicationMailer
   default from: 'admin@translantics.com'
 
   def new_quote
     @quote = params[:quote]
-    mail(to: @quote.quoter.email, subject: "A new quote(#{@quote.total_gross_weight} Kg) has been received")
+    mail(to: @quote.quoter.email, subject: "Your quote(#{@quote.total_gross_weight} Kg) has been received")
   end
 
   def admin_email
     @quote = params[:quote]
-    mail(to: @quote.quoter.email, subject: "A new quote(#{@quote.total_gross_weight} Kg) has been received")
+    mail(to: @quote.quoter.email, subject: "A new quote has been received")
   end
 end
