7775bf3c48cb3dc43f72d67c36e19d3e9c2c33e4
Yusif Badmus <yusif.badmus@gmail.com>
2024-03-17 17:38:05 +0000

    add quotes from frontend app

--- app/controllers/api/v1/quotes_controller.rb
+++ app/controllers/api/v1/quotes_controller.rb
@@ -9,7 +9,7 @@ module Api
         service.perform
         @quote = service.quote
 
-        if @quote.save
+        if service.errors.blank?
           QuoteMailer.with(quote: @quote).new_quote.deliver_later
           render_success('success', @quote, QuoteSerializer)
         else
@@ -20,7 +20,9 @@ module Api
       private
 
       def quote_params
-        params.require(:quote).permit(:frieght_type, :height, :length, :width, :message, :status, :total_gross_weight, :incoterm_id, departure_attributes: %i[city country], destination_attributes: %i[city country], quoter_attributes: %i[name email phone])
+        params.permit(:frieght_type, :height, :length, :width, :message, :status, :total_gross_weight, :incoterm_id, :departure, :destination, :name, :email, :phone, departure_attributes: %i[city country], destination_attributes: %i[city country], quoter_attributes: %i[name email phone])
+
+        # params.require(:quote).permit(:frieght_type, :height, :length, :width, :message, :status, :total_gross_weight, :incoterm_id, :departure, :destination, :name, :email, :phone, departure_attributes: %i[city country], destination_attributes: %i[city country], quoter_attributes: %i[name email phone])
       end
     end
   end
--- app/services/create_quote_service.rb
+++ app/services/create_quote_service.rb
@@ -4,16 +4,17 @@ class CreateQuoteService < BaseService
   attr_reader :quote, :errors
 
   def initialize(params:)
+    binding
     @quote_params = params
-    @quoter_params = params[:quoter_attributes]
-    @departure_params = params[:departure_attributes]
-    @destination_params = params[:destination_attributes]
+    @quoter_params = params[:quoter_attributes].presence || { name: params[:name], email: params[:email] }
+    @departure_params = params[:departure_attributes].presence || { city: params[:departure] }
+    @destination_params = params[:destination_attributes].presence || { city: params[:destination] }
     @errors = []
     @quote = {}
   end
 
   def perform
-    @quote = Quote.new(@quote_params)
+    @quote = Quote.new(@quote_params.except(:departure, :destination, :name, :phone, :email))
     @quote.departure = departure
     @quote.destination = destination
     @quote.quoter = quoter
