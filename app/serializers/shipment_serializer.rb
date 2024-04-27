e322878949a748a7a9003e1c99f22909d61c840d
Yusif Badmus <yusif.badmus@gmail.com>
2024-04-26 21:40:47 -0400

    add shipping history attribute

--- app/serializers/shipment_serializer.rb
+++ app/serializers/shipment_serializer.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class ShipmentSerializer < ActiveModel::Serializer
-  attributes :id, :length, :width, :height, :declared_value, :description, :dutiable, :frieght_type, :status, :tracking_number, :shipper, :receiver, :incoterm, :destination, :departure, :location, :shipping_information, :primary_address, :weight, :quantity, :created_at, :updated_at
+  attributes :id, :length, :width, :height, :declared_value, :description, :dutiable, :frieght_type, :status, :tracking_number, :shipper, :receiver, :incoterm, :destination, :departure, :location, :shipping_information, :primary_address, :weight, :quantity, :shipping_history, :created_at, :updated_at
 
   def incoterm
     object.incoterm.as_json
@@ -49,5 +49,15 @@ class ShipmentSerializer < ActiveModel::Serializer
 
   def created_at
     object.created_at.strftime('%m/%d/%Y, %T')
+  end
+
+  def shipping_history
+    object.audits.map do |audit|
+      {
+        date_time: audit.created_at.strftime('%m/%d/%Y %I:%M%p'),
+        status: audit.action == 'create' ? ShippingDetail.statuses.key(audit.audited_changes['status']).tr('_', ' ')&.capitalize : ShippingDetail.statuses.key(audit.audited_changes['status']&.last).tr('_', ' ')&.capitalize,
+        location: audit.action == 'create' ? Location.find_by(id: audit.audited_changes['location_id']).city : Location.find_by(id: audit.audited_changes['location_id'].last.to_i).city
+      }
+    end
   end
 end
